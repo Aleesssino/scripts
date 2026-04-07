793b690d4fc3b36d356333b27b2fc3eda0b7e399
Aleš Gábor <97041873+Aleesssino@users.noreply.github.com>
2026-04-07 11:16:57 +0200

    Fix conditional check for program name input

--- C-skeleton/see.sh
+++ C-skeleton/see.sh
@@ -2,7 +2,9 @@
 # Prompt for the program name
 read -p "Enter the name of the C program: " programName
 
-[[ "$programName" == "." || " " ]] && programName=$(basename "$PWD")
+if [[ "$programName" == "." || -z "$programName" ]]; then
+  programName=$(basename "$PWD")
+fi
 
 #  Create Makefile
 echo -e "CC=gcc\nCFLAGS= -g -Wall -Wextra -std=c11 -pedantic\n\n$programName: $programName.c\n\t\$(CC) \$(CFLAGS) -o $programName $programName.c" >Makefile
